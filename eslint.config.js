7a4fde2ef9b7511eb5c7d62645fc035939dacc80
Fernando Pasik <fernando@pasik.com.ar>
2024-09-20 22:13:23 +0100

    style: remove eslint

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import prettier from 'eslint-config-prettier';
-import ymlPlugin from 'eslint-plugin-yml';
-
-export default [
-  { ignores: ['galaxy/', 'venv/'] },
-  ...ymlPlugin.configs['flat/recommended'],
-  ...ymlPlugin.configs['flat/prettier'],
-  prettier,
-];
