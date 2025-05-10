a01b4c2ae3cede14ca8d566d0f37936a463a43ff
Fernando Pasik <fernando@pasik.com.ar>
2025-05-10 17:37:21 +0100

    fix: path for pyenv dependencies

--- machines/_common/default.nix
+++ machines/_common/default.nix
@@ -25,6 +25,16 @@
     zlib
   ];
 
+  environment.shellInit = ''
+    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${
+      pkgs.lib.makeSearchPath "pkgconfig" [
+        pkgs.zlib
+        pkgs.openssl
+        pkgs.bzip2
+      ]
+    }
+  '';
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
