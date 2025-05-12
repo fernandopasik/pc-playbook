7c68155fc8be5012b0321f2d818c35c051edbad1
Fernando Pasik <fernando@pasik.com.ar>
2025-05-12 23:14:00 +0000

    feat: switch to nixos unstable channel

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Fernando's NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs =
