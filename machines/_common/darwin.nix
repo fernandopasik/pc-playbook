c5545fb4b237890cf6a3909286cbe613d2836096
Fernando Pasik <fernando@pasik.com.ar>
2025-05-16 18:45:18 +0100

    feat: mac apps

--- machines/_common/darwin.nix
+++ machines/_common/darwin.nix
@@ -3,6 +3,22 @@
 {
   homebrew.enable = true;
 
+  homebrew.casks = [
+    # Essentials
+    "google-chrome"
+    "stats"
+    "the-unarchiver"
+    "vlc"
+
+    # QuickLook plugins
+    "qlcolorcode"
+    "qlmarkdown"
+    "qlstephen"
+    "quicklook-csv"
+    "quicklook-json"
+    "webpquicklook"
+  ];
+
   system.primaryUser = "fernando";
   system.stateVersion = 6;
 }
